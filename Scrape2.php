f7c8955e627f8bd4a2520d938e40b68a0150673f
Pragyaditya Das <pragyaditya.das94@gmail.com>
2014-07-15 22:06:05 +0530

    Create Scrape2.php

---
+++ Scrape2.php
@@ -0,0 +1,24 @@
+<?php
+
+error_reporting(E_ERROR); //ignoring any warnings
+
+
+/*here is a php function used to get the html source from the webpage using cURL*/
+function curlGet($url) {
+	$ch = curl_init($url); //starting cURL
+	curl_setopt($ch, CURLOPT_RETURNTRANSFER, true); //write the response to a variable
+    curl_setopt($ch, CURLOPT_URL, $url);
+    $results = curl_exec($ch);
+    curl_close($ch);
+	return $results;
+}
+
+$packtPage = curlGet('http://www.espncricinfo.com/ci/engine/match/scores/live.html');
+//search for images
+foreach($packtPage->find("img") as $element)
+	echo $element->src . '<br>';
+
+//find all links
+foreach($packtPage->find("a") as $element)
+echo $element->href . '<br>'; 
+?>
